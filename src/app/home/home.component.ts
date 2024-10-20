6eefd8a7e10b9a15f1a65d2c6f95674117cb0d29
BeEz <98581184+Beez1@users.noreply.github.com>
2024-10-20 12:47:16 +0100

    Refactor carlist and home components for improved styling and responsiveness

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -24,19 +24,25 @@ interface Car {
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit {
+getUniqueYears(): any {
+throw new Error('Method not implemented.');
+}
   cars: Car[] = [];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.fetchCars();
+  }
+
+  private fetchCars(): void {
     this.http.get<Car[]>('http://localhost:8080/cars').subscribe({
-      next: (data) => {
-        this.cars = data;
-        console.log('Cars fetched:', this.cars);
+      next: (cars: Car[]) => {
+        this.cars = cars;
       },
-      error: (error) => {
+      error: (error: any) => {
         console.error('Error fetching cars:', error);
       }
     });
-  } 
+  }
 }
